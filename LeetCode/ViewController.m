ec491af3a7ef2364619b97869b2b66da28dcf46c
fanchuan <bob.fan@uxin.com>
2021-04-20 17:48:31 +0800

    快排

--- LeetCode/ViewController.m
+++ LeetCode/ViewController.m
@@ -24,9 +24,15 @@
     NSInteger index = [self binarySearch:list target:target];
     NSLog(@"二分查找 %@",@(index));
 
+    NSArray *randomList = @[@7,@5,@2,@9,@6,@8,@7,@999];
+    //快排
+    [self quickSort:[randomList mutableCopy] left:0 right:randomList.count - 1];
+    //冒泡
+    [self bubbleSort:randomList];
 }
 
 #pragma mark - 二分查找
+//时间复杂度为O(logN)
 - (NSInteger)binarySearch:(NSArray *)list target:(NSInteger)target {
     NSInteger left = 0;
     NSInteger right = list.count - 1;
@@ -44,7 +50,59 @@
     return -1;
 }
 
+#pragma mark - 快速排序
+/**
+ 分治法
+ 1.将大于中间值的数都放到右边
+ 2.将小于中间值的数都放到左边
+ */
+- (void)quickSort:(NSMutableArray *)list left:(NSInteger)left right:(NSInteger)right {
+    if (left >= right) {
+        //当左侧索引 >= 右侧索引时,说明遍历完成
+        NSLog(@"快速排序 %@",list);
+        return;
+    }
+    NSInteger i = left;
+    NSInteger j = right;
+    NSInteger key_value = [self countFromArray:list index:i];
+    while (i < j) {
+        while (i < j) {
+            NSInteger j_value = [self countFromArray:list index:j];
+            if (j_value >= key_value) {
+                j--;
+            } else {
+                break;
+            }
+        }
+        if (i == j) {
+            //说明选择的起始数值已经是最小的数了
+            break;
+        }
+        list[i] = list[j];
 
+        while (i < j) {
+            NSInteger i_value = [self countFromArray:list index:i];
+            if (i_value <= key_value) {
+                i++;
+            } else {
+                break;
+            }
+        }
+        if (i == j) {
+            //说明选择的起始数值已经是最小的数了
+            break;
+        }
+        list[j] = list[i];
+    }
+    list[i] = @(key_value);
+    [self quickSort:list left:left right:i-1];
+    [self quickSort:list left:i+1 right:right];
+}
+
+#pragma mark - 冒泡排序
+- (void)bubbleSort:(NSArray *)list {
+
+}
 
 #pragma mark - Private
 - (NSInteger)countFromArray:(NSArray *)array index:(NSInteger)index {
